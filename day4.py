0ba15636ed142376f46a1b6540c496920f6dd564
thlabbe <labbe.thomas@gmail.com>
2023-12-04 10:59:19 +0100

    Day 4 part 2

--- day4.py
+++ day4.py
@@ -1,10 +1,9 @@
-file = "./input4.txt"
-#file = "./test4.txt"
+FILE = "./input4.txt"
+# FILE = "./test4.txt"
 
 
 def load():
-
-    with open(file, "rt") as text:
+    with open(FILE, "rt") as text:
         return text.read().splitlines()
 
 
@@ -24,18 +23,38 @@ def preprocess(data):
             if n > '':
                 plays_nums.append(int(n))
 
-        liste.append( {'id':number,
-                       'winning': win_nums,
-                       'hand': plays_nums})
+        liste.append({'id': number,
+                      'winning': win_nums,
+                      'hand': plays_nums,
+                      'score': 0})
     return liste
-def part2():
-    return 0
+
+
+def part2(scored): # : FIXME trop de bricolages sur les indices +/- 1
+    res = 0
+    scores = []
+    for x in range(1 + len(scored)):
+        scores.append(0)
+
+    for x in range(1, len(scored)):
+        scores[x] += 1
+        g = scored[x - 1]
+        s = scored[g['score']]
+
+        for y in range(1 + x, x + s['id']):
+            scores[y] += scores[x]
+
+    for s in scores:
+        res += s
+    return res + 1 # FIXME : c'est moche
+
 
 def part1():
     data = load()
     games = preprocess(data)
-    #{'id':number, 'winning': win_nums, 'hand': plays_nums}
+    # {'id':number, 'winning': win_nums, 'hand': plays_nums}
     total = 0
+    scored = []
     for g in games:
         count = 0
         win_num = g['winning']
@@ -43,15 +62,16 @@ def part1():
         for w in win_num:
             if w in hand:
                 count += 1
+        g['score'] = count
+        scored.append(g)
+
         if count > 0:
             total += 2 ** (count - 1)
 
-    return total
-
+    return total, scored
 
 
 if __name__ == '__main__':
-
-    print("part1 : ",part1())
-    print("part2 : ", part2())
-
+    res1, data2 = part1()
+    print("part1 : ", res1)
+    print("part2 : ", part2(data2))
